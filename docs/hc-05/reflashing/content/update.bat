e328985c96f368c4891da62aec57411b453870c5
unknown <z80@cortex-m3.(none)>
2016-01-25 16:44:45 -0800

    a

--- docs/hc-05/reflashing/content/update.bat
+++ docs/hc-05/reflashing/content/update.bat
@@ -1,14 +1,14 @@
 PATH=%PATH%;C:\Programs\CSR\BlueSuite-2.4
-#pscli query data.psr data.psq 
-#BlueFlashCmd HC05
-#pscli merge data.psr
+rem pscli query data.psr data.psq 
+rem BlueFlashCmd HC05
+rem pscli merge data.psr
 
-# Backup chip settings
-# pscli -trans "SPITRANS=USB SPIPORT=1" dump pskeys-backup.psr
-# Save firmware backup (only for chips with flash, backup will include PS keys):
+rem Backup chip settings
+rem pscli -trans "SPITRANS=USB SPIPORT=1" dump pskeys-backup.psr
+rem Save firmware backup (only for chips with flash, backup will include PS keys):
 blueflashcmd -trans "SPITRANS=USB SPIPORT=1" -dump firmware-backup
 
-# Flash firmware from files csr-fw-backup.xpv and csr-fw-backup.xdv:
+rem Flash firmware from files csr-fw-backup.xpv and csr-fw-backup.xdv:
 blueflashcmd -trans "SPITRANS=USB SPIPORT=1" HC05
 
 
